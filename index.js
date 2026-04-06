0550186fcbe9ccfa9dba3aee65e82446910fe073
Waleed Mohamed <mr.waleed.it@gmail.com>
2026-04-07 00:18:20 +0200

    استبدال index.js بالكود المحسن لعرض index.html

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// جعل المجلد الحالي متاح كـ static files
-app.use(express.static(path.join(__dirname)));
+// تحديد مجلد ثابت (public) لعرض الملفات
+app.use(express.static(path.join(__dirname, 'public')));
 
-// عند الدخول على الرابط الرئيسي "/" يعرض index.html
+// عند الدخول على الرابط الرئيسي "/" يعرض index.html من مجلد public
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // تشغيل السيرفر
